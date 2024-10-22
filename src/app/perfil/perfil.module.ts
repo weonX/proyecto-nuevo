f72cbc0aace4d97d40d3038160595e8055b8836a
epinares <ed.pinares@duocuc.cl>
2024-10-21 21:02:03 -0300

    configuración page "perfil"

--- src/app/perfil/perfil.module.ts
+++ src/app/perfil/perfil.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { PerfilPageRoutingModule } from './perfil-routing.module';
-
 import { PerfilPage } from './perfil.page';
 
 @NgModule({
--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { CameraService } from '../services/camera.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
-export class PerfilPage implements OnInit {
+export class PerfilPage {
+  profilePhotoUrl: string | null = null;
 
-  constructor() { }
+  constructor(
+    private menuCtrl: MenuController,
+    private cameraService: CameraService
+  ) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.menuCtrl.enable(true);
   }
 
+  async takePicture() {
+    const photo = await this.cameraService.takePicture();
+    if (photo) {
+      this.profilePhotoUrl = photo;
+    }
+  }
 }
